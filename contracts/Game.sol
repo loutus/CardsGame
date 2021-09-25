6c8fe671cd823f73552990bf49178cbc2f98f8da
r.nouripoor <r.nouripoor@gmail.com>
2021-09-25 08:00:11 +0330

    side cards game almost finished

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,47 +1,91 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 import "./Register.sol";
 
 contract Game is Register{
 
-    mapping(uint256 => address) cardToOwner;
-
+    address public link;
     uint256 public cardsRemaining;
-    uint256 public x;
-    uint256 public y;
+    uint256 public baseFee;
+    uint256 public totalPrize;
+    bool public marketIsOpen;
+    
 
-    event BuyCard(address indexed buyer, uint256 cardNumber);
+    event BuyCard(address indexed buyer, uint256 numberOfCards);
 
-    constructor (uint256 _x, uint256 _y) {
+    constructor (uint256 _x, uint256 _y,uint256 _baseFee, address marketAddr, address generatorAddr, address ownerAddr) {
+        link = address(this);
         x = _x;
         y = _y;
         cardsRemaining = x * y;
+        baseFee = _baseFee;
+        generatorAddress = generatorAddr;
+        marketAddress = marketAddr;
+        ownerAddress = ownerAddr;
     }
 
+    modifier onlyGenerator() {
+        require(msg.sender == marketAddress);
+        _;
+    }
 
-    function ownerOfCard(uint256 _cardNum) public view returns(address) {
-        require(cardToOwner[_cardNum] != address(0), "this card does not have an owner");
-        return cardToOwner[_cardNum];
+    function end() public onlyGenerator returns(address) {
+        _closeMarket();
+        _updateScores();
+        address winner = _whoWins();
+        _win(winner);
+        totalPrize = 0;
+        increaseCredit(winner, totalPrize);
+        return winner;
     }
 
     function buyCards(uint256 numberOfCards) public {
         require (numberOfCards > 0 , "buy some cards !!!");
-        require(numberOfCards <= cardsRemaining, "not enough cards available. set lesser number.");
-        require(numberOfCards <= 5, "you can only buy 5 cards.");
+        //continue untill 70% of cards bought.
+        require(cardsRemaining - numberOfCards >= (x * y) * 3 / 10, "not enough cards available. set lesser number.");
+        //maximum cards random for every person 
+        require(numberOfCards <= cardsRemaining * 35 / 100, "you can only buy 20% of remaining cards.");
+
+        uint256 fee = numberOfCards * baseFee;
+
+        require(checkCredit(msg.sender) >= fee, "not enough eth, please charge your credit.");
 
         //first card will be selected by random
-        uint256 lastCard = _randomCard();
+        uint256 lastCard = _randomCard();       
+        _getCard(msg.sender, lastCard);
 
         //other cards will be selected near to last card
-        for(uint i=1; i < numberOfCards; i++){
-            lastCard = _randomSide(lastCard);
+        for(uint i = 1; i < numberOfCards; i++) {
+            lastCard = _randomSide(lastCard);       
+            _getCard(msg.sender, lastCard);
+        }
+
+        cardsRemaining -= numberOfCards;
+        decreaseCredit(msg.sender, fee);
+        totalPrize += fee;
+
+        emit BuyCard(msg.sender, numberOfCards);
+
+        _updateScores();
+
+        if(cardsRemaining == (x * y) * 3 / 10) {
+            _openMarket();
+            _ownRestOfCards();
         }
     }
 
 
-    function _randomCard() private returns(uint256) {
+    function _openMarket() private {
+        marketIsOpen = true;
+    }
+
+    function _closeMarket() private {
+        marketIsOpen = false;
+    }
+
+    function _randomCard() private view returns(uint256) {
         //request for a random index from existing cards
         uint256 randIndex = (_randomHash() % cardsRemaining);
 
@@ -55,18 +99,11 @@ contract Game is Register{
             selectedCard ++;
         } while(counter <= randIndex);
         selectedCard --;
-
-
-        //make sure the card would not be selected again
-        cardsRemaining --;
-        cardToOwner[selectedCard] = msg.sender;        
-        _getCard(msg.sender, selectedCard);
     
         return selectedCard;
     }
 
-
-    function _randomSide(uint256 _cardNum) private returns(uint256) {
+    function _randomSide(uint256 _cardNum) private view returns(uint256) {
 
         uint256[4] memory sidesAvailable;
 
@@ -106,14 +143,6 @@ contract Game is Register{
 
             selectedCard = sidesAvailable[randIndex];
         }
-
-
-        //make sure the card would not be selected again
-        cardsRemaining --;
-        cardToOwner[selectedCard] = msg.sender;        
-        _getCard(msg.sender, selectedCard);
-
-        
         
         return selectedCard;
     }
--- contracts/Register.sol
+++ contracts/Register.sol
@@ -1,50 +1,148 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 
 contract Register{
 
     struct User {
         string username;
-        uint256[] cards;
+        uint256 credit;
+        uint256 score;
         bool won;
     }
 
-    mapping(address => User) users;
+    uint256 public x;
+    uint256 public y;
+    address public marketAddress;
+    address public generatorAddress;
+    address public ownerAddress;
+
+    address[] addressList;
+    
+    mapping(address => User) addrToUser;
+    mapping(uint256 => address) cardToOwner;
+
 
     event SignIn(address indexed userAddress, string username);
     event GetCard(address userAddress, uint256 cardnumber);
     event Win(address userAddress);
 
-    function username(address userAddr) public view returns(string memory) {
-        require(bytes(users[msg.sender].username).length > 0, "user has not sign in yet");
-        return users[userAddr].username;
-    }
-
-    function userCards(address userAddr) public view returns(uint256[] memory) {
-        require(bytes(users[msg.sender].username).length > 0, "user has not sign in yet");
-        return users[userAddr].cards;
+    modifier onlyMarket() {
+        require(msg.sender == marketAddress);
+        _;
     }
 
     function signIn(string memory _username) public {
-        require(bytes(users[msg.sender].username).length == 0, "user has signed in before");
+        require(bytes(addrToUser[msg.sender].username).length == 0, "user has signed in before");
+        
+        ////number of users should be limited
+        require(addressList.length <= x * y / ((x + y) / 2) + 2, "number of users limit");
+
         require(bytes(_username).length > 0, "set a username");
 
-        users[msg.sender].username = _username;
+        addressList.push(msg.sender);
+        addrToUser[msg.sender].username = _username;
 
         emit SignIn(msg.sender, _username);
     }
 
-    function _getCard(address _userAddr, uint256 _cardNum) internal {
-        require(bytes(users[msg.sender].username).length > 0, "user has not sign in yet");
-        users[_userAddr].cards.push(_cardNum);
+    function username(address userAddr) public view returns(string memory) {
+        require(bytes(addrToUser[msg.sender].username).length > 0, "user has not sign in yet");
+        return addrToUser[userAddr].username;
+    }
+
+
+////////////about cards
+
+    function ownerOf(uint256 _cardNum) public view returns(address) {
+        require(_cardNum < x * y, "this card does not exist.");
+        require(cardToOwner[_cardNum] != address(0), "this card does not have an owner");
+        return cardToOwner[_cardNum];
+    }
+
+    // function userCards(address _userAddr) public returns(uint[] memory) {}
+
+    function _getCard(address _userAddr, uint256 _cardNum) public onlyMarket {
+        require(bytes(addrToUser[_userAddr].username).length > 0, "user has not sign in yet");
+        // addrToUser[_userAddr].cards.push(_cardNum);
+        cardToOwner[_cardNum] = _userAddr;
         emit GetCard(_userAddr, _cardNum);
     }
 
+    function _ownRestOfCards() internal {
+        for(uint256 i = 0; i < x * y; i++){
+            if(cardToOwner[i] == address(0)){
+                cardToOwner[i] = generatorAddress;
+            }
+        }
+    }
+////////////about scores
+    
+    function userScore(address userAddr) public view returns(uint256) {
+        return addrToUser[userAddr].score;
+    }
+
+    function _resetScores() private {
+        for(uint256 i = 0; addressList[i] != address(0); i++) {
+            addrToUser[addressList[i]].score = 0;
+        }
+    }
+    
+    function _updateScores() internal {
+        _resetScores();
+
+        for(uint256 i = 0; i < x * y; i++){
+            if(cardToOwner[i] != address(0)){
+                //one score for each card
+                addrToUser[cardToOwner[i]].score++;
+                //one score for right side
+                if(cardToOwner[i] == cardToOwner[i + 1]) {addrToUser[cardToOwner[i]].score++;}
+                //one score for bottom side
+                if(cardToOwner[i] == cardToOwner[i + x]) {addrToUser[cardToOwner[i]].score++;}
+            }
+        }
+    }
+
+    function _whoWins() internal view returns(address) {
+        address winner = addressList[0];
+        for(uint256 i = 1; addressList[i] != address(0); i++) {
+            if(addrToUser[addressList[i]].score > addrToUser[addressList[i - 1]].score) {winner = addressList[i];}
+        }
+        return winner;
+    }
+
     function _win(address _userAddr) internal {
-        require(bytes(users[msg.sender].username).length > 0, "user has not sign in yet");
-        users[_userAddr].won = true;
+        require(bytes(addrToUser[msg.sender].username).length > 0, "user has not sign in yet");
+        addrToUser[_userAddr].won = true;
         emit Win(_userAddr);
+    }
+
+////////////about credit
+
+    function charge() public payable {
+        require(msg.value > 0, "zero charging value");
+        addrToUser[msg.sender].credit += msg.value;
+    }
+
+    function withdraw() public {
+        uint amount = addrToUser[msg.sender].credit;
+        addrToUser[msg.sender].credit = 0;
+        address payable reciever;
+        if(msg.sender != ownerAddress) {reciever = payable(msg.sender);}
+        else {reciever = payable(ownerAddress);}
+        reciever.transfer(amount);
+    }
+
+    function checkCredit(address _userAddr) public view returns(uint256) {
+        return addrToUser[_userAddr].credit;
+    }
+
+    function increaseCredit(address _userAddr, uint256 _value) public onlyMarket {
+        addrToUser[_userAddr].credit += _value;
+    }
+
+    function decreaseCredit(address _userAddr, uint256 _value) public onlyMarket {
+        addrToUser[_userAddr].credit -= _value;
     }
 }
